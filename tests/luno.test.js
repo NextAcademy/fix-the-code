f715a54fde13673a54bf6001fe0601cdb90012c7
Josh Teng <josh@dev.server>
2022-11-04 10:36:35 +0000

    test for luno response error

--- tests/luno.test.js
+++ tests/luno.test.js
@@ -2,6 +2,7 @@ const MOCK_PRICE = 99
 const MOCK_JSON_RESP = { last_trade: MOCK_PRICE }
 
 global.fetch = jest.fn(() => Promise.resolve({
+  status: 200,
   json: () => Promise.resolve(MOCK_JSON_RESP)
 }));
 
